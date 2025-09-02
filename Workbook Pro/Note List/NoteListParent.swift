bdd8990a292b457985d3e2bb13392b7af29e2dee
TopScrech <sergei_saliukov@icloud.com>
2025-09-02 08:27:06 +0200

    shortcut for opening the settings

--- Workbook Pro/Note List/NoteListParent.swift
+++ Workbook Pro/Note List/NoteListParent.swift
@@ -39,6 +39,7 @@ struct NoteListParent: View {
                 } label: {
                     Image(systemName: "gear")
                 }
+                .keyboardShortcut("S")
             }
     }
     
