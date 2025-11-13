762593f1e36593d3e96be85a56fe8a13cb8dfc43
TopScrech <sergei_saliukov@icloud.com>
2025-11-13 13:06:57 +0100

    upgrade & removed 2 unused files

--- Workbook Pro/Storage.swift
+++
@@ -1,7 +0,0 @@
-import SwiftUI
-
-final class ValueStorage: ObservableObject {
-    @AppStorage("show_status_bar") var showStatusBar = true
-    @AppStorage("show_nav_bar") var showNavBar = true
-    @AppStorage("enable_group_activities") var enableGroupActivities = false
-}
